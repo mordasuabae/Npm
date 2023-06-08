b1b46b4370342a43a1643dee1c72bbaaf832b6fb
alaricomanjate21@gmal.com <alaricomanjate21@gmail.com>
2023-06-08 12:12:53 -0700

    Fix bug and change map style to fullscreean

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-alert('Boom')
+alert('Boom');
+
+const {Loader} = require('google-maps'); //without typescript
+ 
+const loader = new Loader('AIzaSyCWHOe5v5kRHd_Rs1VUThFzIPuEbTcsgrs');
+ 
+
+loader.load().then(function (google) {
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+    });
+});
